b96ad6808774d71fd37a2c8949670c983a7b39c2
slottwo <41028091+slottwo@users.noreply.github.com>
2024-03-15 12:10:38 -0300

    soe initial file

---
+++ sieve-of-eratosthenes/0.c
@@ -0,0 +1,51 @@
+/**
+ * @file 0.c
+ * @authors slottwo, GersonFeDutra
+ * @brief Sieve of Eratosthenes
+ * @version 0.1
+ * @date 2024-03-15
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/** @brief 1 GB */
+#define N 1073741824
+
+int main(int argc, char const *argv[])
+{
+    // MPI.h Initialization
+
+    int wsize, rank;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &wsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Status status;
+
+    bool *primes;
+
+    int size = N / wsize + (N % wsize != 0);
+    bool *non_primes = (bool *)calloc(size, sizeof(bool));
+
+    /** @brief Initial time stamp */
+    double t_init;
+    if (rank == 0)
+        t_init = MPI_Wtime();
+
+    /* INICIO */
+
+    /* FIM */
+
+    if (rank == 0)
+        printf("Δt: %f\n", MPI_Wtime() - t_init);
+
+    free(primes);
+
+    MPI_Finalize();
+    return 0;
+}
