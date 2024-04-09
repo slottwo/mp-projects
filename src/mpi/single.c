0c325eaace82dac26733d2191fb3cdd0f08b61c7
Pedro Galvão <41028091+slottwo@users.noreply.github.com>
2024-04-09 01:47:03 -0300

    reapply fixes

--- src/mpi/single.c
+++ src/mpi/single.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 	non_primes[1] = true;
 
 	clock_t clk;
-	
+
 	if (rank == ROOT)
 		clk = clock();
 
@@ -73,27 +73,30 @@ int main(int argc, char *argv[])
 
 	/* End */
 
-	clk = clock() - clk;
+	if (rank == ROOT)
+	{
+		clk = clock() - clk;
 
-	FILE *log;
-	log = fopen("bin/log/mpi_single", "a+");
-	if (log == NULL)
-		exit(1);
-	fprintf(log, "%d %d\n", N, clk);
-	fclose(log);
+		FILE *log;
+		log = fopen("bin/log/mpi_single", "a+");
+		if (log == NULL)
+			exit(1);
+		fprintf(log, "%d %d\n", N, clk);
+		fclose(log);
 
-	// FILE *out;
-	// out = fopen("bin/out/mpi_single", "w");
-	// if (out == NULL)
-	//     exit(1);
-	// for (i = 0; i < _L; i++)
-	//     if (!non_primes[i])
-	//         fprintf(out, "%d ", i);
-	// fclose(out);
+		// FILE *out;
+		// out = fopen("bin/out/mpi_single", "w");
+		// if (out == NULL)
+		//     exit(1);
+		// for (i = 0; i < _L; i++)
+		//     if (!non_primes[i])
+		//         fprintf(out, "%d ", i);
+		// fclose(out);
+
+		free(NON_PRIMES);
+	}
 
 	free(non_primes);
-	if (rank == ROOT)
-		free(NON_PRIMES);
 
 	MPI_Finalize();
 	return 0;
